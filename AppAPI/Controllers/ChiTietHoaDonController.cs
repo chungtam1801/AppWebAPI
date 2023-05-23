df7eb8ae17efe7fd4b1ec1f786b674e04179a61a
Dat <Dat@DAT>
2023-05-23 21:45:31 +0700

    hd hdct

--- AppAPI/Controllers/ChiTietHoaDonController.cs
+++ AppAPI/Controllers/ChiTietHoaDonController.cs
@@ -1,11 +1,91 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppData.IRepositories;
+using AppData.Models;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ChiTietHoaDonController : ControllerBase
+    public class HoaDonController : ControllerBase
     {
+        private readonly IAllRepository<HoaDon> repos;
+        AssignmentDBContext context = new AssignmentDBContext();
+        DbSet<HoaDon> hoadon;
+        public HoaDonController()
+        {
+            hoadon = context.HoaDons;
+            AllRepository<HoaDon> all = new AllRepository<HoaDon>(context, hoadon);
+            repos = all;
+        }
+
+        // GET: api/<HoaDOnController>
+        [HttpGet]
+        public IEnumerable<HoaDon> Get()
+        {
+            return repos.GetAll();
+        }
+        // GET api/<HoaDonController>/5
+        [HttpGet("{id}")]
+        public HoaDon Get(Guid id)
+        {
+            return repos.GetAll().FirstOrDefault(x => x.ID == id);
+        }
+
+        // POST api/<HoaDonController>
+        [HttpPost("Create-HoaDon")]
+
+        public bool Post(DateTime ngayTao, DateTime ngayThanhToan, string tenNguoiNhan, string sdt, string email, string diaChi, int tienShip, string phuongthucthanhtoan, int trangThaiGiaoHang, Guid idNguoiDung, Guid idVoucher)
+        {
+            HoaDon hoaDon = new HoaDon();
+            hoaDon.ID = Guid.NewGuid();
+            hoaDon.NgayTao = ngayTao;
+            hoaDon.NgayThanhToan = ngayThanhToan;
+            hoaDon.SDT = sdt; ;
+            hoaDon.Email = email;
+            hoaDon.DiaChi = diaChi;
+            hoaDon.TienShip = tienShip;
+            hoaDon.PhuongThucThanhToan = phuongthucthanhtoan;
+            hoaDon.TrangThaiGiaoHang = trangThaiGiaoHang;
+            hoaDon.IDNguoiDung = idNguoiDung;
+            hoaDon.IDVoucher = idVoucher;
+            return repos.Add(hoaDon);
+        }
+        [HttpPut("Update-HoaDon")]
+        public bool UpdateHoaDon(Guid id, DateTime ngayTao, DateTime ngayThanhToan, string tenNguoiNhan, string sdt, string email, string diaChi, int tienShip, string phuongthucthanhtoan, int trangThaiGiaoHang, Guid idNguoiDung, Guid idVoucher)
+        {
+            var hoaDon = repos.GetAll().FirstOrDefault(h => h.ID == id);
+            if (hoadon != null)
+            {
+                hoaDon.NgayTao = ngayTao;
+                hoaDon.NgayThanhToan = ngayThanhToan;
+                hoaDon.SDT = sdt; ;
+                hoaDon.Email = email;
+                hoaDon.DiaChi = diaChi;
+                hoaDon.TienShip = tienShip;
+                hoaDon.PhuongThucThanhToan = phuongthucthanhtoan;
+                hoaDon.TrangThaiGiaoHang = trangThaiGiaoHang;
+                hoaDon.IDNguoiDung = idNguoiDung;
+                hoaDon.IDVoucher = idVoucher;
+                return repos.Update
+                    (hoaDon);
+            }
+            else { return false; }
+        }
+
+        [HttpDelete("Delete-HoaDon")]
+
+        public bool DeleteHoaDon(Guid id)
+        {
+            var hoaDon = repos.GetAll().FirstOrDefault(p => p.ID == id);
+            if (hoaDon != null)
+            {
+                return repos.Delete(hoaDon);
+            }
+            else { return false; }
+        }
+
     }
 }
--- AppAPI/Controllers/HoaDonController.cs
+++ AppAPI/Controllers/HoaDonController.cs
@@ -1,11 +1,98 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppData.IRepositories;
+using AppData.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AppData.Repositories;
 
 namespace AppAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class HoaDonController : ControllerBase
+    public class ChiTietHoaDonController : ControllerBase
     {
+        private readonly IAllRepository<ChiTietHoaDon> repos;
+        AssignmentDBContext context = new AssignmentDBContext();
+        DbSet<ChiTietHoaDon> chiTietHoaDons;
+
+        public ChiTietHoaDonController()
+        {
+            chiTietHoaDons = context.ChiTietHoaDons;
+            AllRepository<ChiTietHoaDon> all = new AllRepository<ChiTietHoaDon>(context, chiTietHoaDons);
+            repos = all;
+        }
+        // GET: api/<ChiTietHoaDOnController>
+        [HttpGet]
+        public IEnumerable<ChiTietHoaDon> Get()
+        {
+            return repos.GetAll();
+        }
+        [HttpGet("{id}")]
+        public ChiTietHoaDon Get(Guid id)
+        {
+            return repos.GetAll().FirstOrDefault(x => x.ID == id);
+        }
+
+        [HttpPost("Create-ctHoaDon")]
+
+        public bool Post(int donGia, int soLuong, int trangThai, Guid idBienThe, Guid idHoaDon)
+        {
+            ChiTietHoaDon chiTietHoaDon = new ChiTietHoaDon();
+            chiTietHoaDon.ID = Guid.NewGuid();
+            chiTietHoaDon.DonGia = donGia;
+            chiTietHoaDon.SoLuong = soLuong;
+            chiTietHoaDon.TrangThai = trangThai;
+            chiTietHoaDon.IDBienThe = idBienThe;
+            chiTietHoaDon.IDHoaDon = idHoaDon;
+            return repos.Add(chiTietHoaDon);
+        }
+
+        // PUT api/<ChiTietHoaDonController>/5
+        [HttpPut("Update-ChiTietHoaDon")]
+
+        public bool UpdatectHoaDon(Guid id, int donGia, int soLuong, int trangThai, Guid idBienThe, Guid idHoaDon)
+        {
+            var chiTietHoaDon = repos.GetAll().FirstOrDefault(p => p.ID == id);
+            if (chiTietHoaDon != null)
+            {
+                chiTietHoaDon.DonGia = donGia;
+                chiTietHoaDon.SoLuong = soLuong;
+                chiTietHoaDon.TrangThai = trangThai;
+                chiTietHoaDon.IDBienThe = idBienThe;
+                chiTietHoaDon.IDHoaDon = idHoaDon;
+                return repos.Add(chiTietHoaDon);
+            }
+            else { return false; }
+        }
+        // DELETE api/<ChiTietHoaDonController>/5
+        [HttpDelete("Delete-ChiTietHoaDon")]
+        public bool DeleteChiTietHoaDon(Guid id)
+        {
+            var chiTietHoaDon = repos.GetAll().FirstOrDefault(p => p.ID == id);
+            if (chiTietHoaDon != null)
+            {
+                return repos.Delete(chiTietHoaDon);
+            }
+            else { return false; }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
