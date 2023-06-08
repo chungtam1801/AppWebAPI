bd8ce38bc89f65311b2b4385f08ce28d5566515c
lamtran <lamtran6131@gmail.com>
2023-06-08 17:59:49 +0700

    Them 4 controler

--- AppAPI/Controllers/SanPhamController.cs
+++ AppAPI/Controllers/SanPhamController.cs
@@ -25,30 +25,37 @@ namespace AppAPI.Controllers
         {
             return repos.GetAll();
         }
+        
+        [HttpGet("{id}")]
+        public SanPham GetId(Guid id)
+        {
+            return repos.GetAll().FirstOrDefault(x => x.ID == id);
+        }
+
 
         // GET api/<SanPhamController>/5
-        [HttpGet("{name}")]
-        public List<SanPham> Get(string name)
-        {
-            return repos.GetAll().Where(x=>x.Ten.Contains(name)).ToList();
-        }
+        //[HttpGet("{name}")]
+        //public List<SanPham> Get(string name)
+        //{
+        //    return repos.GetAll().Where(x=>x.Ten.Contains(name)).ToList();
+        //}
 
         // POST api/<SanPhamController>
         [HttpPost("create-san-pham")]
-        public bool Post(string ten,Guid idLoaiSP,string moTa)
+        public bool Post(string ten,Guid idLoaiSP,string moTa,int trangthai)
         {
             SanPham sanPham = new SanPham();
             sanPham.ID = Guid.NewGuid();
             sanPham.Ten = ten;
             sanPham.IDLoaiSP = idLoaiSP;
             sanPham.MoTa = moTa;
-            sanPham.TrangThai = 1;
+            sanPham.TrangThai =trangthai ;
             return repos.Add(sanPham);
         }
 
         // PUT api/<SanPhamController>/5
         [HttpPut("{id}")]
-        public bool Put(Guid id, string ten, Guid idLoaiSP, string moTa)
+        public bool Put(Guid id, string ten, Guid idLoaiSP, string moTa,int trangthai)
         {
             var sp = repos.GetAll().FirstOrDefault(x=>x.ID == id);
             if(sp != null)
@@ -56,6 +63,7 @@ namespace AppAPI.Controllers
                 sp.Ten = ten;
                 sp.IDLoaiSP = idLoaiSP;
                 sp.MoTa = moTa;
+                sp.TrangThai=trangthai ;
                 return repos.Update(sp);
             }
             else
--- AppData/Models/ThuocTinh.cs
+++ AppData/Models/ThuocTinh.cs
@@ -1,8 +1,11 @@
-﻿namespace AppData.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppData.Models
 {
     public class ThuocTinh
     {
         public Guid ID { get; set; }
+        [StringLength(50, ErrorMessage = "Ten thuoc tinh khong duoc dai qua 50 ki tu.")]
         public string Ten { get; set; }
         public DateTime NgayTao { get; set; }
         public int TrangThai { get; set; }
