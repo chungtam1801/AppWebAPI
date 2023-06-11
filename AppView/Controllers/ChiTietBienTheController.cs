f0c309e11300bbf44779a2d00de6add0cefcfd99
dungtqph28227@fpt.edu.vn <dungtqph28227@fpt.edu.vn>
2023-06-11 16:35:13 +0700

    SanPhamDone

--- AppView/Controllers/ChiTietBienTheController.cs
+++ AppView/Controllers/ChiTietBienTheController.cs
@@ -1,6 +1,8 @@
 ﻿using AppData.Models;
+using AppView.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using NuGet.Protocol;
 
 namespace AppView.Controllers
 {
@@ -11,13 +13,29 @@ namespace AppView.Controllers
         {
             httpClients = new HttpClient();
         }
+        public IActionResult getID()
+        {
+            return RedirectToAction("Create");
+        }
         public async Task<IActionResult> GetAllChiTietBienThe()
         {
             string apiUrl = "https://localhost:7021/api/ChiTietBienThe";
             var response = await httpClients.GetAsync(apiUrl);
             string apiData = await response.Content.ReadAsStringAsync();
             var CTBienThe = JsonConvert.DeserializeObject<List<ChiTietBienThe>>(apiData);
-
+            ViewBag.CTBienThe = CTBienThe;
+            //Giatri LoaiSP
+            string apiUrlLoaiSP = "https://localhost:7021/api/LoaiSP";
+            var responseLoaiSP = await httpClients.GetAsync(apiUrlLoaiSP);
+            string apiDataLoaiSP = await responseLoaiSP.Content.ReadAsStringAsync();
+            var loaiSP = JsonConvert.DeserializeObject<List<LoaiSP>>(apiDataLoaiSP);
+            ViewBag.LoaiSP = loaiSP;
+            //Giatri ThuocTinh
+            string apiUrlThuocTinh = "https://localhost:7021/api/ThuocTinh";
+            var responseThuocTinh = await httpClients.GetAsync(apiUrlThuocTinh);
+            string apiDataThuocTinh = await responseThuocTinh.Content.ReadAsStringAsync();
+            var thuocTinh = JsonConvert.DeserializeObject<List<ThuocTinh>>(apiDataThuocTinh);
+            ViewBag.ThuocTinh = thuocTinh;
             //Giatri getall
             string apiUrlGiaTri = "https://localhost:7021/api/GiaTri";
             var responseGT = await httpClients.GetAsync(apiUrlGiaTri);
@@ -37,49 +55,39 @@ namespace AppView.Controllers
             var SanPham = JsonConvert.DeserializeObject<List<SanPham>>(apiDatasp);
             ViewBag.SanPham = SanPham;
 
-            return View(CTBienThe);
+            return View(BienThe);
         }
-
-        // Them nhanh gia tri (cha cua chi tiet bien the)
-        public async Task<IActionResult> CreateGiaTri()
+        public async Task<IActionResult> Create()
         {
-            string apiUrlThuoctinh = "https://localhost:7021/api/ThuocTinh";
-            var responseTT = await httpClients.GetAsync(apiUrlThuoctinh);
-            string apiDataTT = await responseTT.Content.ReadAsStringAsync();
-            var thuocTinh = JsonConvert.DeserializeObject<List<ThuocTinh>>(apiDataTT);
+            string apiUrl = "https://localhost:7021/api/ChiTietBienThe";
+            var response = await httpClients.GetAsync(apiUrl);
+            string apiData = await response.Content.ReadAsStringAsync();
+            var CTBienThe = JsonConvert.DeserializeObject<List<ChiTietBienThe>>(apiData);
+            ViewBag.CTBienThe = CTBienThe;
+            //Giatri LoaiSP
+            string apiUrlLoaiSP = "https://localhost:7021/api/LoaiSP";
+            var responseLoaiSP = await httpClients.GetAsync(apiUrlLoaiSP);
+            string apiDataLoaiSP = await responseLoaiSP.Content.ReadAsStringAsync();
+            var loaiSP = JsonConvert.DeserializeObject<List<LoaiSP>>(apiDataLoaiSP);
+            ViewBag.LoaiSP = loaiSP;
+            //Giatri ThuocTinh
+            string apiUrlThuocTinh = "https://localhost:7021/api/ThuocTinh";
+            var responseThuocTinh = await httpClients.GetAsync(apiUrlThuocTinh);
+            string apiDataThuocTinh = await responseThuocTinh.Content.ReadAsStringAsync();
+            var thuocTinh = JsonConvert.DeserializeObject<List<ThuocTinh>>(apiDataThuocTinh);
             ViewBag.ThuocTinh = thuocTinh;
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> CreateGiaTri(GiaTri giaTri)
-        {
-            var url = $"https://localhost:7021/api/GiaTri/Create_GiaTri?ten={giaTri.Ten}&idThuocTinh={giaTri.IdThuocTinh}";
-            var response = await httpClients.PostAsync(url, null);
-            if (response.IsSuccessStatusCode) return RedirectToAction("Create");
-            return View();
-        }
-        //
-        // them nhan thuoc tinh (cha cua gia tri)
-        public async Task<IActionResult> CreateThuocTinh()
-        {
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> CreateThuocTinh(ThuocTinh thuoctinh)
-        {
-            DateTime ngaytao = thuoctinh.NgayTao;
-            var url = $"https://localhost:7021/api/ThuocTinh/Create_ThuocTinh?ten={thuoctinh.Ten}&ngaytao={ngaytao.ToString("MM-dd-yyyy")}&trangthai={thuoctinh.TrangThai}";
-            var response = await httpClients.PostAsync(url, null);
-            if (response.IsSuccessStatusCode) return RedirectToAction("CreateGiaTri");
-            return View();
-            //repos.Add(thuoctinh);
-            //return RedirectToAction("GetAllThuocTinh");
-        }
-
-        //
-        // Them Nhanh Bien The( cha cua chi tiet bien the)
-        public async Task<IActionResult> CreateBienThe()
-        {
+            //Giatri getall
+            string apiUrlGiaTri = "https://localhost:7021/api/GiaTri";
+            var responseGT = await httpClients.GetAsync(apiUrlGiaTri);
+            string apiDataGT = await responseGT.Content.ReadAsStringAsync();
+            var giaTri = JsonConvert.DeserializeObject<List<GiaTri>>(apiDataGT);
+            ViewBag.GiaTri = giaTri;
+            //BienThe getall
+            string apiUrlBT = "https://localhost:7021/api/BienThe";
+            var responseBT = await httpClients.GetAsync(apiUrlBT);
+            string apiDataBT = await responseBT.Content.ReadAsStringAsync();
+            var BienThe = JsonConvert.DeserializeObject<List<BienThe>>(apiDataBT);
+            ViewBag.BienThe = BienThe;
             //SanPham Getall
             string apiUrlsp = "https://localhost:7021/api/SanPham";
             var responsesp = await httpClients.GetAsync(apiUrlsp);
@@ -89,8 +97,51 @@ namespace AppView.Controllers
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> CreateBienThe(BienThe bienthe, IFormFile Anh)
+        public async Task<IActionResult> Create(AllSanPhamBienTheViewModel sanpham, IFormFile Anh,List<Guid> IDGiaTri)
         {
+            //ctbt
+            string apiUrlCTBT = "https://localhost:7021/api/ChiTietBienThe";
+            var responseCTBT = await httpClients.GetAsync(apiUrlCTBT);
+            string apiDataCTBT = await responseCTBT.Content.ReadAsStringAsync();
+            var CTBienThe = JsonConvert.DeserializeObject<List<ChiTietBienThe>>(apiDataCTBT);
+            ViewBag.CTBienThe = CTBienThe;
+            // LoaiSP
+            string apiUrlLoaiSP = "https://localhost:7021/api/LoaiSP";
+            var responseLoaiSP = await httpClients.GetAsync(apiUrlLoaiSP);
+            string apiDataLoaiSP = await responseLoaiSP.Content.ReadAsStringAsync();
+            var loaiSP = JsonConvert.DeserializeObject<List<LoaiSP>>(apiDataLoaiSP);
+            ViewBag.LoaiSP = loaiSP;
+            // ThuocTinh
+            string apiUrlThuocTinh = "https://localhost:7021/api/ThuocTinh";
+            var responseThuocTinh = await httpClients.GetAsync(apiUrlThuocTinh);
+            string apiDataThuocTinh = await responseThuocTinh.Content.ReadAsStringAsync();
+            var thuocTinh = JsonConvert.DeserializeObject<List<ThuocTinh>>(apiDataThuocTinh);
+            ViewBag.ThuocTinh = thuocTinh;
+            //Giatri getall
+            string apiUrlGiaTri = "https://localhost:7021/api/GiaTri";
+            var responseGT = await httpClients.GetAsync(apiUrlGiaTri);
+            string apiDataGT = await responseGT.Content.ReadAsStringAsync();
+            var giaTri = JsonConvert.DeserializeObject<List<GiaTri>>(apiDataGT);
+            ViewBag.GiaTri = giaTri;
+            //BienThe getall
+            string apiUrlBT = "https://localhost:7021/api/BienThe";
+            var responseBT = await httpClients.GetAsync(apiUrlBT);
+            string apiDataBT = await responseBT.Content.ReadAsStringAsync();
+            var BienThe = JsonConvert.DeserializeObject<List<BienThe>>(apiDataBT);
+            ViewBag.BienThe = BienThe;
+            //SanPham Getall
+            string apiUrlsp = "https://localhost:7021/api/SanPham";
+            var responsesp = await httpClients.GetAsync(apiUrlsp);
+            string apiDatasp = await responsesp.Content.ReadAsStringAsync();
+            var SanPham = JsonConvert.DeserializeObject<List<SanPham>>(apiDatasp);
+            ViewBag.SanPham = SanPham;
+            //them sp
+                Guid idSP = Guid.NewGuid(); 
+                var urlthemSP = $"https://localhost:7021/api/SanPham/create-san-pham?id={idSP}&ten={sanpham.Ten}&idLoaiSP={sanpham.IDLoaiSP}&moTa={sanpham.MoTa}&trangthai={sanpham.TrangThai}";
+                var responsethemSP = await httpClients.PostAsync(urlthemSP, null);
+                string aapiDatasp = await responsesp.Content.ReadAsStringAsync();
+                
+            //them bienthe
             if (Anh != null && Anh.Length > 0) // Kiểm tra đường dẫn phù hợp
             {
                 // thực hiện việc sao chép ảnh đó vào wwwroot
@@ -100,99 +151,27 @@ namespace AppView.Controllers
                 var stream = new FileStream(path, FileMode.Create); // Tạo 1 filestream để tạo mới
                 Anh.CopyTo(stream); // Copy ảnh vừa dc chọn vào đúng cái stream đó
                 // Gán lại giá trị link ảnh (lúc này đã nằm trong root cho thuộc tính description)
-                bienthe.Anh = Anh.FileName;
+                sanpham.Anh = Anh.FileName;
             }
-            string apiUrlsp = "https://localhost:7021/api/SanPham";
-            var responsesp = await httpClients.GetAsync(apiUrlsp);
-            string apiDatasp = await responsesp.Content.ReadAsStringAsync();
-            var SanPham = JsonConvert.DeserializeObject<List<SanPham>>(apiDatasp);
-            ViewBag.SanPham = SanPham;
-            DateTime ngaytao = bienthe.NgayTao;
-
-            var url = $"https://localhost:7021/api/BienThe/create-bien-the?IdSanPham={bienthe.IDSanPham}&SoLuong={bienthe.SoLuong}&GiaBan={bienthe.GiaBan}&NgayTao={ngaytao.ToString("MM-dd-yyyy")}&Anh={bienthe.Anh}";
-            var response = await httpClients.PostAsync(url, null);
-            string apiData = await response.Content.ReadAsStringAsync();
-
-            if (response.IsSuccessStatusCode) return RedirectToAction("Create");
-            return View();
-        }
-        //
-        // Them nhanh San Pham ( cha cua bien the)
-        public async Task<IActionResult> CreateSanPham()
-        {
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> CreateSanPham(SanPham sanpham)
-        {
-            var url = $"https://localhost:7021/api/SanPham/create-san-pham?ten={sanpham.Ten}&idLoaiSP={sanpham.IDLoaiSP}&moTa={sanpham.MoTa}&trangthai={sanpham.TrangThai}";
-            var response = await httpClients.PostAsync(url, null);
-            if (response.IsSuccessStatusCode) return RedirectToAction("CreateBienThe");
-            return View();
-        }
-        //
-        // Them nhanh LoaiSp (cha cua San Pham)
-        public async Task<IActionResult> CreateLoaiSP()
-        {
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> CreateLoaiSP(LoaiSP loaiSP)
-        {
-
-            var url = $"https://localhost:7021/api/LoaiSP/create-loaisp?ten={loaiSP.Ten}&idLoaiSPCha={loaiSP.IDLoaiSPCha}&trangthai={loaiSP.TrangThai}";
-            var response = await httpClients.PostAsync(url, null);
-            if (response.IsSuccessStatusCode) return RedirectToAction("CreateSanPham");
-            return View();
-        }
-        //
-        public async Task<IActionResult> Create()
-        {
-            //Giatri getall
-            string apiUrlGiaTri = "https://localhost:7021/api/GiaTri";
-            var responseGT = await httpClients.GetAsync(apiUrlGiaTri);
-            string apiDataGT = await responseGT.Content.ReadAsStringAsync();
-            var giaTri = JsonConvert.DeserializeObject<List<GiaTri>>(apiDataGT);
-            ViewBag.GiaTri = giaTri;
-            //BienThe getall
-            string apiUrlBT = "https://localhost:7021/api/BienThe";
-            var responseBT = await httpClients.GetAsync(apiUrlBT);
-            string apiDataBT = await responseBT.Content.ReadAsStringAsync();
-            var BienThe = JsonConvert.DeserializeObject<List<BienThe>>(apiDataBT);
-            ViewBag.BienThe = BienThe;
-            //SanPham Getall
-            string apiUrlsp = "https://localhost:7021/api/SanPham";
-            var responsesp = await httpClients.GetAsync(apiUrlsp);
-            string apiDatasp = await responsesp.Content.ReadAsStringAsync();
-            var SanPham = JsonConvert.DeserializeObject<List<SanPham>>(apiDatasp);
-            ViewBag.SanPham = SanPham;
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> Create(ChiTietBienThe chiTietBienThe)
-        {
-            //Giatri getall
-            string apiUrlGiaTri = "https://localhost:7021/api/GiaTri";
-            var responseGT = await httpClients.GetAsync(apiUrlGiaTri);
-            string apiDataGT = await responseGT.Content.ReadAsStringAsync();
-            var giaTri = JsonConvert.DeserializeObject<List<GiaTri>>(apiDataGT);
-            ViewBag.GiaTri = giaTri;
-            //BienThe getall
-            string apiUrlBT = "https://localhost:7021/api/BienThe";
-            var responseBT = await httpClients.GetAsync(apiUrlBT);
-            string apiDataBT = await responseBT.Content.ReadAsStringAsync();
-            var BienThe = JsonConvert.DeserializeObject<List<BienThe>>(apiDataBT);
-            ViewBag.BienThe = BienThe;
-            //SanPham Getall
-            string apiUrlsp = "https://localhost:7021/api/SanPham";
-            var responsesp = await httpClients.GetAsync(apiUrlsp);
-            string apiDatasp = await responsesp.Content.ReadAsStringAsync();
-            var SanPham = JsonConvert.DeserializeObject<List<SanPham>>(apiDatasp);
-            ViewBag.SanPham = SanPham;
-            var url = $"https://localhost:7021/api/ChiTietBienThe/create-CTBienThe?idBienThe={chiTietBienThe.IDBienThe}&idGiaTri={chiTietBienThe.IDGiaTri}";
-            var response = await httpClients.PostAsync(url, null);
-            if (response.IsSuccessStatusCode) return RedirectToAction("GetAllChiTietBienThe");
-            return View();
+            
+            DateTime ngaytao = sanpham.NgayTao;
+            Guid idBienThe = Guid.NewGuid();
+            var urlBienThe = $"https://localhost:7021/api/BienThe/create-bien-the?id={idBienThe}&IdSanPham={idSP}&SoLuong={sanpham.SoLuong}&GiaBan={sanpham.GiaBan}&NgayTao={ngaytao.ToString("MM-dd-yyyy")}&Anh={sanpham.Anh}";
+            var responseBienThe = await httpClients.PostAsync(urlBienThe, null);
+            //List<Guid> idgiatri = ViewBag.IDgiatri;
+            //return Content(idgiatri.ToString());
+            foreach (var item in IDGiaTri)
+            {
+                var url = $"https://localhost:7021/api/ChiTietBienThe/create-CTBienThe?idBienThe={idBienThe}&idGiaTri={item}";
+                var response = await httpClients.PostAsync(url, null);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return View();
+                }
+            }
+            
+             return RedirectToAction("GetAllChiTietBienThe");
+            
         }
         [HttpGet]
         public async Task<IActionResult> Edit(Guid id)
